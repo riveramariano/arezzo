70c5e52cd089aaf01b07498bd169dafe388bca30
Mariano Rivera <rivveramariano@gmail.com>
2022-07-12 19:06:18 -0600

    Update Libro Functionality
    Add the ability to update any libro.

--- app/src/main/java/com/arezzo/ui/libro/UpdateLibroFragment.kt
+++ app/src/main/java/com/arezzo/ui/libro/UpdateLibroFragment.kt
@@ -1,38 +1,57 @@
 package com.arezzo.ui.libro
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
 import com.arezzo.R
 import com.arezzo.databinding.FragmentAddLibroBinding
+import com.arezzo.databinding.FragmentUpdateLibroBinding
 import com.arezzo.model.Libro
 import com.arezzo.viewmodel.LibroViewModel
 
-class AddLibroFragment : Fragment() {
+class UpdateLibroFragment : Fragment() {
     private lateinit var libroViewModel: LibroViewModel
 
-    private var _binding: FragmentAddLibroBinding? = null
+    private var _binding: FragmentUpdateLibroBinding? = null
     private val binding get() = _binding!!
+
+    private val args by navArgs<UpdateLibroFragmentArgs>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentAddLibroBinding.inflate(inflater, container, false)
+        _binding = FragmentUpdateLibroBinding.inflate(inflater, container, false)
 
         libroViewModel = ViewModelProvider(this).get(LibroViewModel::class.java)
 
-        binding.btnAgregarLibro.setOnClickListener { addLibro() }
+        binding.btnActualizarLibro.setOnClickListener { updateLibro() }
+
+        binding.etNombre.setText(args.libro.nombre)
+        binding.etUnidades.setText(args.libro.unidades.toString())
+        binding.etAutor.setText(args.libro.autor)
+
+        // setHasOptionsMenu(true)
 
         return binding.root
     }
 
-    private fun addLibro() {
+//    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+//        inflater.inflate(R.menu.delete_menu, menu)
+//    }
+//
+//    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+//        if (item.itemId == R.id.menu_delete) {
+//            deleteLugar()
+//        }
+//        return super.onOptionsItemSelected(item)
+//    }
+
+    private fun updateLibro() {
         var nombre = binding.etNombre.text.toString()
         var unidades = binding.etUnidades.text.toString()
         var autor = binding.etAutor.text.toString()
@@ -50,19 +69,19 @@ class AddLibroFragment : Fragment() {
                 return
             }
             var libro = Libro(
-                "",
+                args.libro.id,
                 nombre,
                 unidades.toInt(),
                 autor,
                 "",
                 ""
             )
-            libroViewModel.addLibro(libro)
-            Toast.makeText(requireContext(), "Libro Agregado", Toast.LENGTH_SHORT).show()
+            libroViewModel.updateLibro(libro)
+            Toast.makeText(requireContext(), "Libro Actualizado", Toast.LENGTH_SHORT).show()
         } else {
             Toast.makeText(requireContext(), "Faltan Datos", Toast.LENGTH_SHORT).show()
             return
         }
-        findNavController().navigate(R.id.action_addLibroFragment_to_nav_libro)
+        findNavController().navigate(R.id.action_updateLibroFragment_to_nav_libro)
     }
 }
