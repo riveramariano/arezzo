42c275ef26a727656ff212997f702220ac08ebb4
renji10 <emilio.uribe2001@gmail.com>
2022-08-23 21:55:37 -0600

    Logica de imagenes para sucursales
     Se genera la logica de agregar imagenes para la sucursales

--- app/src/main/java/com/arezzo/ui/libro/UpdateLibroFragment.kt
+++ app/src/main/java/com/arezzo/ui/libro/UpdateLibroFragment.kt
@@ -1,6 +1,7 @@
 package com.arezzo.ui.libro
 
 import android.app.AlertDialog
+import android.net.Uri
 import android.os.Bundle
 import android.view.*
 import androidx.fragment.app.Fragment
@@ -33,6 +34,8 @@ class UpdateLibroFragment : Fragment() {
         binding.etAutor.setText(args.libro.autor)
 
         binding.btnActualizarLibro.setOnClickListener { updateLibro() }
+
+
 
         setHasOptionsMenu(true)
 
--- app/src/main/java/com/arezzo/ui/sucursal/AddSucursalFragment.kt
+++ app/src/main/java/com/arezzo/ui/sucursal/AddSucursalFragment.kt
@@ -1,5 +1,7 @@
 package com.arezzo.ui.sucursal
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -12,12 +14,22 @@ import com.arezzo.R
 import com.arezzo.databinding.FragmentAddSucursalBinding
 import com.arezzo.model.Sucursal
 import com.arezzo.viewmodel.SucursalViewModel
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
+import java.text.SimpleDateFormat
+import java.util.*
 
 class AddSucursalFragment : Fragment() {
     private lateinit var sucursalViewModel: SucursalViewModel
 
     private var _binding: FragmentAddSucursalBinding? = null
     private val binding get() = _binding!!
+
+    private var imageUri: Uri?=null
+    private var imageString: String =" "
+    private var PICK_IMAGE_REQUEST =71
+    private var firebaseStore: FirebaseStorage?= null
+    private var storageReference: StorageReference?=null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -28,6 +40,12 @@ class AddSucursalFragment : Fragment() {
         sucursalViewModel = ViewModelProvider(this).get(SucursalViewModel::class.java)
 
         binding.btnAgregarSucursal.setOnClickListener { addSucursal() }
+
+        binding.btnEscogerSucursalAdd.setOnClickListener{
+            launchGallery() }
+
+        binding.btnSubirSucursalAdd.setOnClickListener{
+            SubirImagen() }
 
         return binding.root
     }
@@ -63,5 +81,38 @@ class AddSucursalFragment : Fragment() {
             return
         }
         findNavController().navigate(R.id.action_addSucursalFragment_to_nav_sucursal)
+    }
+
+    private fun launchGallery() {
+        val intent = Intent()
+        intent.type = "image/*"
+        intent.action = Intent.ACTION_GET_CONTENT
+        startActivityForResult(Intent.createChooser(intent, "Escoger Imagen"), PICK_IMAGE_REQUEST);
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        this.imageUri = data?.data
+        binding.PreviewSucursalAdd.setImageURI(this.imageUri)
+    }
+
+    private fun SubirImagen() {
+        val name = binding.etGerente.toString()
+
+        val formatter = SimpleDateFormat("yyyy_MM_dd_HH_mm_ss", Locale.getDefault())
+        val now = Date()
+        val fileName = "_" + formatter.format(now)
+        val storageReference = FirebaseStorage.getInstance().getReference("images/$name$fileName")
+
+        this.imageUri?.let {
+            storageReference
+                .putFile(it)
+                .addOnFailureListener {
+                    Toast.makeText(requireContext(),getString(R.string.fail_save),Toast.LENGTH_SHORT).show()
+                }.addOnSuccessListener { taskSnapshot ->
+                    binding.PreviewSucursalAdd.setImageURI(null)
+                    Toast.makeText(requireContext(),getString(R.string.success_save),Toast.LENGTH_SHORT).show()
+                }
+
+        }
     }
 }
